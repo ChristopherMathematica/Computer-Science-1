b5c2893561beb4e2cc51440a3ccb8ada9d6699d5
ChristopherMathematica <58499404+ChristopherMathematica@users.noreply.github.com>
2022-07-10 14:51:10 -0400

    Update and rename chrisyoneklab4.cpp to SumBetweenNumbers.cpp

---
+++ SumBetweenNumbers.cpp
@@ -0,0 +1,29 @@
+﻿//CSC114-651
+//Student: Christopher Yonek (700642859)
+//Assignment: Lab 4 - Create a program that sums the numbers between 0 and a specified number.
+//Date: October 2018
+
+#include "pch.h"
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int num = 0;
+	int sum = 0;
+
+	cout << "Enter how many numbers you want to sum\n";
+	cin >> num;
+
+	while (num < 0){
+		cout << "Please enter positive integer only,\n";
+		cin >> num;
+	}
+	for (int i = 1; i <= num; i++){
+		sum += i;
+	}
+	cout << "The total sum is:" << sum;
+	_getch();
+	return 0;
+}
+
